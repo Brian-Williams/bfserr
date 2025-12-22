ed3e816e30cdfe35595e8ef1be592bfc1898c67e
Brian-Williams <15200179+Brian-Williams@users.noreply.github.com>
2025-12-22 14:41:04 -0500

    Add errors.As comparison to example_test.go

--- example_test.go
+++ example_test.go
@@ -12,16 +12,29 @@ type customErr struct{ msg string }
 func (e customErr) Error() string { return e.msg }
 
 func ExampleAsType() {
+	// Create a tree of errors
 	err := fmt.Errorf("wrap 1: %w",
 		fmt.Errorf("wrap 2: %w",
 			customErr{"target too deep"},
 		),
 	)
 	err2 := fmt.Errorf("wrap 1: %w", customErr{"target"})
+	joinedErr := errors.Join(err, err2)
 
-	if e, ok := bfserr.AsType[customErr](errors.Join(err, err2)); ok {
-		fmt.Println("found:", e)
+	// Use bfserr.AsType to search for a specific type of error in the tree
+	// finding the shallowest matching error first
+	if e, ok := bfserr.AsType[customErr](joinedErr); ok {
+		fmt.Println("bfserr found:", e)
 	}
+
+	// Use errors.As to search for a specific type of error in the tree
+	// finding the deepest matching error first
+	var errAs customErr
+	if errors.As(errors.Join(err, err2), &errAs) {
+		fmt.Println("errors.As found:", errAs)
+	}
+
 	// Output:
-	// found: target
+	// bfserr found: target
+	// errors.As found: target too deep
 }
